48fb49e48af8f906fe0053152a781847dd44534b
Stepanka Neumannova <stepanka.neumannova20@gmail.com>
2024-02-13 20:14:47 +0100

    syntax correction

--- Secondary_table_project_SQL_final.sql
+++ Secondary_table_project_SQL_final.sql
@@ -1,34 +1,32 @@
-
-CREATE TABLE t_stepanka_neumannova_project_SQL_secondary_final (
-WITH basic AS (
+CREATE TABLE t_stepanka_neumannova_project_SQL_secondary_final AS (
+	WITH basic AS (
+		SELECT 
+			continent,
+			country AS country_eu, 
+			currency_code,
+			currency_name 
+		FROM countries c 
+		WHERE continent = 'Europe'
+	),
+	basic_2 AS (
+		SELECT 
+		 	b.*,
+		 	e.*
+		FROM basic b
+		LEFT JOIN economies e
+			ON b.country_eu = e.country
+	)
 	SELECT 
-		continent,
-		country AS country_eu, 
+		country_eu,
 		currency_code,
-		currency_name 
-	FROM countries c 
-	WHERE continent = 'Europe'
-),
-basic_2 AS (
-	SELECT 
-	 	b.*,
-	 	e.*
-	FROM basic b
-	LEFT JOIN economies e
-		ON b.country_eu = e.country
-)
-SELECT 
-	country_eu,
-	currency_code,
-	currency_name,
-	`year`,
-	round (GDP,0) AS GDP_round,
-	gini,
-	population
-FROM basic_2
-WHERE country IS NOT NULL
-	AND `year` BETWEEN '2006' AND '2018'
-ORDER BY country_eu ASC,
-		`year` ASC
-)
-;
+		currency_name,
+		`year`,
+		round (GDP,0) AS GDP_round,
+		gini,
+		population
+	FROM basic_2
+	WHERE country IS NOT NULL
+		AND `year` BETWEEN '2006' AND '2018'
+	ORDER BY country_eu ASC,
+			`year` ASC
+);
