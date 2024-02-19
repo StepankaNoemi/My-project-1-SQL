d1ada90bc690f8165087158620b912886f76f9d6
Stepanka Neumannova <stepanka.neumannova20@gmail.com>
2024-02-19 19:25:25 +0100

    eidting-changes in primary table,ln28 calculation

--- Task no.4_project_ SQL_final.sql
+++ Task no.4_project_ SQL_final.sql
@@ -1,23 +1,23 @@
 WITH basic_payroll AS (
 	SELECT 
 		a.*,
-		LAG(a.average_payroll_year) OVER (PARTITION BY a.industry_branch_code_final ORDER BY a.`year`) AS previous_payroll
+		LAG(a.average_payroll_value) OVER (PARTITION BY a.industry_branch_code_final ORDER BY a.`year`) AS previous_payroll
 	FROM (
 		SELECT 
 			DISTINCT final_year AS `year`,
 			CONCAT(type_name_payroll,'  ','v',' ',unit_payroll) AS type_payroll,
 			industry_branch_code_final,
 			branch_name,
-			average_payroll_year 
+			average_payroll_value 
 		FROM t_stepanka_neumannova_project_sql_primary_final tsnpspf
-		WHERE industry_branch_code_final = 'all_branches'
+		WHERE industry_branch_code_final = 'všechna odvětví'
 		ORDER BY industry_branch_code_final
 	)a
 ),
 increase_all_branch AS (
 	SELECT 
 		*,
-		round ((average_payroll_year - previous_payroll)/previous_payroll*100, 2) AS annual_percent_increase_all_branch
+		round ((average_payroll_value - previous_payroll)/previous_payroll*100, 2) AS annual_percent_increase_all_branch
 	FROM basic_payroll
 	WHERE previous_payroll IS NOT NULL 		
 )
@@ -25,7 +25,7 @@ SELECT
 	cp.`year`,
 	cp.annual_percent_increase_all_branch,
 	ai.annual_increase_percent_all_food,	
-	(cp.annual_percent_increase_all_branch - ai.annual_increase_percent_all_food) AS difference
+	(ai.annual_increase_percent_all_food - cp.annual_percent_increase_all_branch) AS difference
 FROM increase_all_branch cp
 JOIN (
 	SELECT
